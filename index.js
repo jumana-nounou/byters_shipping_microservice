81e332d562d0739897d75d97f53c8a7a7f42d9ba
jumana-nounou <jumana.nounou@student.giu-uni.de>
2022-06-06 01:59:16 +0200

    init

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- const express = require('express');
+const express = require('express');
  const axios = require('axios');
  const cors = require("cors");
  const port = 3000;
@@ -16,62 +16,48 @@ const ordId = require("mongodb").ObjectId;
 
 
  
-//  app.get('/', async (req,res) => {
-//    const db = await mongoClient();
-//    if (!db) res.status(500).send('Systems Unavailable');
+ app.get('/', async (req,res) => {
+   const db = await mongoClient();
+   if (!db) res.status(500).send('Systems Unavailable');
  
-//    const { data } = await axios.get('https://goweather.herokuapp.com/weather/california');
-//    await db.collection('weather').insertOne(data);
+   const { data } = await axios.get('https://goweather.herokuapp.com/weather/california');
+   await db.collection('weather').insertOne(data);
  
-//    return res.send(data);
-// //  });
-// let myquery = { _id: ObjectId( req.params.id )};
-// db_connect
-//     .collection("shippingInfo")
-//     .findOne(myquery, function (err, result) {
-//       if (err) throw err;
-// app.get('/shipments/:orderId', async (req, res) => {
-//   if (!db) res.status(500).send("No db connection");
- 
-//   const { orderId } = req.params;
-//   const results = await db.collection('shipments').findOne({ orderId });
-//   return res.status(200).send(results);
-// });
+   return res.send(data);
+ });
 
 
 
+ var o=4;
 
-
-//     });
-
-
- 
  app.get('/shipments/:orderId', async (req, res) => {
  
   const db = await mongoClient();
   if (!db) res.status(500).send("No db connection");
-  const {orderId}=req.params.mongoClient.ordId;
-  //ordId=req.params
+  //const {orderId}=req.params.mongoClient.ordId;
+  var i=Number(req.params.orderId)
   //const { orderId } = req.params;
   //const orderId  = { _id: ObjectId( req.params.oid )};
- const results = await db.collection('shipments').findOne({orderId}) ;
-   
-  return res.status(200).json(results);
+ const results = await db.collection('shipments').findOne({"orderNo":i}) ;
+  console.log(i)
+  console.log(results)
+  //console.log(ordId)
+  return res.status(200).send(results);
 });
 
 
 
- //62968653276f02397ac4c61b
 app.post('/shipments', async (req, res) => {
   const db = await mongoClient();
   if (!db) res.status(500).send("No db connection");
  
-  const { orderId } = req.body;
+  //const { orderId } = req.body;
   const newShipment = {
-    orderId,
+    orderNo:o,
     status: 'CREATED'
   };
-  const results = await db.collection('shippingInfo').insertOne(newShipment);
+  o=o+1;
+  const results = await db.collection('shipments').insertOne(newShipment);
   return res.status(200).send(results);
 });
 
@@ -79,21 +65,51 @@ app.patch('/shipments/:orderId', async (req, res) => {
   const db = await mongoClient();
   if (!db) res.status(500).send("No db connection");
  
-  const { orderId } = req.params;
-  const result = await db.collection('shipments').findOne({ orderId });
- 
-  // CREATED | SHIPPED | DELIVERED
- 
+ var x= Number(req.params.orderId)
+ console.log(x);
+ const result = await db.collection('shipments').findOne({"orderNo":x}) ;
+
   const updateShipmentStatus = {
     CREATED: 'SHIPPED',
     SHIPPED: 'DELIVERED',
-  }[result.status];
- 
-  const results = await db.collection('shipments').updateOne({ orderId }, { status: updateShipmentStatus });
+  } [result.status];
+
+  const results = await db.collection('shipments').updateOne({ "orderNo":x }, { $set: { "status" : updateShipmentStatus } });
   return res.status(200).send(results);
 });
 
 
+app.delete('/shipments/:orderId', async (req, res) => {
+  const db = await mongoClient();
+  if (!db) res.status(500).send("No db connection");
+ 
+  //const { orderId } = req.body;
+  const Shipment = {
+    orderNo:o,
+    status: 'CREATED'
+  };
+ const results=await db.collection('shipments').deleteOne( Shipment );
+ 
+  return res.status(200).send(results);
+
+});
+
+// law hya cancelled yb2a ana ha return wa law ana khalas ba2et delivered ha3melha returned bardo
+
+app.patch('/shipments/:orderId/Accept', async (req, res) => {
+  const db = await mongoClient();
+  if (!db) res.status(500).send("No db connection");
+ 
+ var x= Number(req.params.orderId)
+ console.log(x);
+ const result = await db.collection('shipments').findOne({"orderNo":x}) ;
+
+  const updateShipmentStatus = {
+    DELIVERED: 'RETURNED'
+  } [result.status];
+  const results = await db.collection('shipments').updateOne({ "orderNo":x }, { $set: { "status" : updateShipmentStatus } });
+  return res.status(200).send(results);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
@@ -101,4 +117,3 @@ app.listen(port, () => {
 
 //const shipmentRoutes=require('./routes/shippment.js');
 //app.use('/shipments',shipmentRoutes);
-
