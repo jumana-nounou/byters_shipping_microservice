f39fabceec4e8159fed9b7d08d339c2d1875dc3d
jumana-nounou <jumana.nounou@student.giu-uni.de>
2022-06-12 20:57:58 +0200

    init

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const BodyParser = require("body-parser");
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
-
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 
 
